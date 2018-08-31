4b918af6db054e34d7eeca9824fcf3f444dc04bf
elwha1 <bghancock@gmail.com>
2018-08-30 22:52:51 -0700

    Started pulling material into calparse module

--- modules/calparse.py
+++ modules/calparse.py
@@ -1,2 +1,32 @@
 
 calcontent = caltree.xpath('//td/text()')
+
+# Build a list with all the entries on the calendar 
+    tree = html.fromstring(cal.content)
+    content = tree.xpath('//td/text()')
+
+    # Search the list for desired keywords
+    for keyword in searchkeys:
+        current_key = r'\b' + keyword + r'\b' 
+
+        for entry in content:
+            date = re.search(dateformat, entry)           
+            match = re.search(current_key, entry, re.IGNORECASE)
+
+            # Grab the dates while scanning; only print if keyword mathc
+            if date:
+                currentdate = date
+            
+            elif match:
+                word_matches += 1
+                judge_matches += 1
+                total_matches += 1            
+                print(currentdate.group())
+                print(entry)
+                
+                # Get hearing information in next entry and print
+                hearing_index = content.index(entry) + 1
+                print(content[hearing_index])
+
+            else:
+                continue
