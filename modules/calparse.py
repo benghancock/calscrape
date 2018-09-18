21dfae74bf16d158f2c199953497009c6ef2e42c
elwha1 <bghancock@gmail.com>
2018-09-17 21:54:04 -0700

    added functions for datetime parsing and basic search

--- modules/calparse.py
+++ modules/calparse.py
@@ -10,7 +10,7 @@ class ParsedCal():
         """Initialize parsed calendar's objects"""
         self.raw = raw 
 
-    def cand_dates(self):
+    def cand_getalldates(self):
         """
         Load the raw body of a CAND calendar and get dates
         """
@@ -23,9 +23,9 @@ class ParsedCal():
             if courtdate:
                 
                 try:
-                    fmt_courtdate = datetime.strptime(
+                    formatted_courtdate = datetime.strptime(
                             courtdate.group(), '%A, %b %d %Y')
-                    self.courtdates.append(fmt_courtdate)
+                    self.courtdates.append(formatted_courtdate)
                
                 except ValueError:
                     pass
@@ -34,6 +34,60 @@ class ParsedCal():
                 continue
 
         return self.courtdates
+
+    def cand_formatdate(self, rawdate):
+        """Return a datetime formatted date"""
+        formatted_courtdate = datetime.strptime(rawdate, '%A, %b %d %Y').date()
+        return formatted_courtdate
+
+    def cand_formattime(self, rawtime):
+        """Return a datetime formatted time"""
+        formatted_courttime = datetime.strptime(rawtime, '%I:%M%p').time()
+        return formatted_courttime
+        
+    def cand_search(self, searchterm):
+        """Return a list of calendar items matching search term"""
+        results = []
+        cal_dateformat = r'\b\w.+\d+.201\d\b'
+        cal_timepattern = r'\d+:\d+\w+(AM|PM)'
+        cal_searchpattern = r'\b' + searchterm + r'\b'
+
+        for entry in self.raw:
+            courtdate = re.search(cal_dateformat, entry)
+            courttime = re.search(cal_timepattern, entry) 
+            match = re.search(cal_searchpattern, entry, re.IGNORECASE)
+            result = ''
+
+            if courtdate:
+                
+                try:
+                    current_courtdate = self.cand_formatdate(courtdate.group())
+
+                except ValueError:
+                    pass
+
+            if courttime:
+                
+                try:
+                    current_courttime = self.cand_formattime(courttime.group())
+
+                except ValueError:
+                    pass
+            
+            if match:
+                match_text = match.group()
+                dateinfo = datetime.combine(current_courtdate,
+                        current_courttime)
+                clean_date = datetime.strftime(dateinfo, '%a %b %Y %I:%M %P')
+
+                result = f'{clean_date}\t{match_text}'
+                results.append(result)
+
+            else:
+                continue
+
+        return results
+        
 
 
 # calcontent = caltree.xpath('//td/text()')
