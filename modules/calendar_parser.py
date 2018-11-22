fcd743f8db49991da6e857aed96abdc74c3ad219
Ben Hancock <bghancock@gmail.com>
2018-11-21 21:38:46 -0800

    began implementing dynamic scraping of judge calendars using index

--- modules/calendar_parser.py
+++ modules/calendar_parser.py
@@ -6,52 +6,50 @@ from bs4 import BeautifulSoup
 class CalendarParser():
     """A court calendar parser object"""
 
-    def __init__(self, base_url):
+    def __init__(self, base_url, calendar_index):
         """Initialize parsed calendar's objects"""
+
+        # Attributes needed for scraping
         self.base_url = base_url
-        self.soup = ''
+        self.calendar_index = calendar_index
+
+        # Attributes of court calendar items
         self.courtdates = []
         self.time_format = '%I:%M%p'
         self.date_format = '%A, %b %d %Y'
         self.cal_dateformat = r'\b\w.+\d+.201\d\b'
         self.cal_timepattern = r'\d+:\d+\w+(AM|PM)'
 
-    def cook_soup(self):
-        """Return a BeautifulSoup object for the base calendar URL"""
-        court_page = requests.get(self.base_url)
-        self.soup = BeautifulSoup(court_page.text, 'lxml')
+    def cook_lxml(self, url):
+        """Return a BeautifulSoup object for a given calendar URL using LXML"""
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'lxml')
 
-        return self.soup
-
-    def more_soup(self, sub_url):
-        """Return a BeautifulSoup object for individual calendars"""
-        calendar_page = requests.get(sub_url)
-        calendar_soup = BeautifulSoup(calendar_page.text, 'lxml')
-
-        return calendar_soup
+        return soup
 
 class CANDParser(CalendarParser):
     """A parser for the California Northern District court"""
 
-    def __init__(self, base_url):
+    def __init__(self, base_url, calendar_index):
         """
         Initialize attributes of the parent class;
         then initialize attributes needed for child class
         """
-        super().__init__(base_url)
+        super().__init__(base_url, calendar_index)
 
     def scrape_calendars(self):
         """Dynamically scrape the calendars listed at the base URL"""
-        self.cook_soup()
+        soup = self.cook_lxml(self.calendar_index)
 
         # Court calendars are organized as a table
-        rows = self.soup.find_all('tr')
+        rows = soup.find_all('tr')
 
         for row in rows:
             judge_name = row.th.a.text.strip()
             url_ending = row.th.a['href'].strip()
-            sub_url = "%s%s" % (self.base_url, url_ending)
-            judge_calendar = self.more_soup(sub_url)
+
+            sub_url = self.base_url + url_ending
+            judge_calendar = self.cook_lxml(sub_url)
 
             print(judge_name)
-            #TODO! Test grabbing correct calendar
+            print(judge_calendar.a.strong)
