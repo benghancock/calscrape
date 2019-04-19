bce359b8687b87c67840e492c6e78f2ad19baf0e
Ben Hancock <bghancock@gmail.com>
2019-04-18 22:07:36 -0700

    laid out structure for class, TODO items

--- modules/hearings_diff.py
+++ modules/hearings_diff.py
@@ -15,3 +15,25 @@ class Hearings():
 
         self.latest_scrape = latest_scrape
         self.prior_scrape = prior_scrape
+
+    def restructure_hearing_data(self):
+        """
+        Transform list of hearing data to dict of lists
+        This makes it easier to compare
+        """
+        # TODO
+
+    def check_for_new(self):
+        """
+        Compare the data from the latest scrape against the prior
+        scrape to determine which hearings are new
+        """
+        # TODO
+
+    def check_status(self):
+        """
+        Check prior scrape against latest scrape to see whether
+        a hearing has gone off calendar or has been changed
+        """
+
+        # TODO
